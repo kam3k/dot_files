2f96a8471b1c81eaa2a0244e8c2c9aa86f3dddc0
Marc Gallant <marc.gallant@quanergy.com>
2016-08-12 13:58:35 -0400

    Re-worked to use stow.

--- setup.sh
+++ setup.sh
@@ -1,36 +1,27 @@
 #!/bin/bash
 
-cd ~
-dir=~/.dot_files
-olddir=~/.dot_files_old
-files="gitconfig vimrc"
-
-if [ -d $olddir ]; then
-    echo "ERROR: $olddir already exists. Delete, move, or rename it."
-    exit
+# Install vundle
+if [ ! -d ~/.vim/bundle/vundle ]; then
+    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 fi
 
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-for file in $files; do
-    if [ -f ~/.$file ]; then
-        mv ~/.$file $olddir
-    fi
-done
-if [ -d ~/.vim ]; then
-    mv ~/.vim $olddir
+# Get fontawesome
+if [ ! -f ~/.fonts/fontawesome-webfont.ttf ]; then
+    curl -fLo ~/.fonts/fontawesome-webfont.ttf --create-dirs https://github.com/FortAwesome/Font-Awesome/raw/master/fonts/fontawesome-webfont.ttf
+    fc-cache -vf ~/.fonts/
 fi
-echo "...done"
 
-echo "Cloning vundle..."
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-echo "...done"
+# Get hack (font)
+if [ ! -f ~/.fonts/Hack-Regular.ttf ]; then
+    curl -fLo ~/.fonts/Hack-Regular.ttf https://github.com/powerline/fonts/blob/master/Hack/Hack-Regular.ttf
+    fc-cache -vf ~/.fonts/
+fi
 
-for file in $files; do
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
-done
+# Make sure stow is installed
+hash stow 2>/dev/null || { echo "Error: stow is not installed. Please install stow first."; exit 1;}
 
-echo "Installing vim bundles..."
-vim +BundleInstall +qall
-echo "...done"
+# Use stow to link dot files in home directory
+cd ~/dot_files/stow
+for app in */; do
+	stow -t ${HOME} $app
+done;
