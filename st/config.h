fb1568d39671450aa0c5d729294ec1756a750e10
Marc Gallant <marc.gallant@quanergy.com>
2017-05-12 11:46:15 -0400

    Turned gaps back on.

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Iosevka:size=18:antialias=true:autohint=true";
+static char font[] = "Iosevka:size=17:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
