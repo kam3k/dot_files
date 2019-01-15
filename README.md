99c80bab40dd97f545890ff4f30710a6310c8d05
Marc Gallant <marc.gallant@quanergy.com>
2019-01-14 22:46:11 -0500

    Updated setup script, added install packages script.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 dot_files
 =========
 
-Various unix dot files including vim configuration. Usually symlinked to the $HOME directory using GNU stow. There is a script to set everything up in the scripts directory.
+Various Unix dot files including vim configuration. Usually symlinked to the $HOME directory using GNU stow. See setup_dot_files.sh for a script to symlink everything automatically.
--- setup_dot_files.sh
+++ setup_dot_files.sh
@@ -26,52 +26,44 @@ if [[ ! -a $(which unzip) ]]; then
   exit 1
 fi
 
-# Set up tmux plugin manager:
+# Set up tmux plugin manager
 mkdir -p ~/.tmux/plugins
 if [ ! -d ~/.tmux/plugins/tpm ]; then
-	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
-# Get the Base16 colour schemes and select colour scheme
-mkdir -p ~/.config
-if [ ! -d ~/.config/base16-shell ]; then
-	git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
-  base16_ocean
-fi
-
-# Set up fonts
-if [ ! -d ~/.fonts ]; then
-  mkdir -p ~/.fonts
+# Set up font directory
+declare fonts=${HOME}/.local/share/fonts
+if [ ! -d ${fonts} ]; then
+  mkdir -p ${fonts}
 fi
 
 # Font Awesome (version 4.7)
-if [ ! -f ~/.fonts/fontawesome-webfont.ttf ]; then
+if [ ! -f ${fonts}/fontawesome-webfont.ttf ]; then
   curl -fLo /tmp/fontawesome.zip https://fontawesome.com/v4.7.0/assets/font-awesome-4.7.0.zip
-	cd /tmp && unzip fontawesome.zip -d fontawesome
-  cp /tmp/fontawesome/font-awesome-4.7.0/fonts/*.ttf ~/.fonts
+  cd /tmp && unzip fontawesome.zip -d fontawesome
+  cp /tmp/fontawesome/font-awesome-4.7.0/fonts/*.ttf ${fonts}
 fi
 
 # Iosevka Term
-if [ ! -f ~/.fonts/iosevka-term-regular.ttf ]; then
+if [ ! -f ${fonts}/iosevka-term-regular.ttf ]; then
   curl -fLo /tmp/iosevka-term.zip https://github.com/be5invis/Iosevka/releases/download/v2.0.0/02-iosevka-term-2.0.0.zip
-	cd /tmp && unzip iosevka-term.zip -d iosevka-term
-  cp /tmp/iosevka-term/ttf/*.ttf ~/.fonts
+  cd /tmp && unzip iosevka-term.zip -d iosevka-term
+  cp /tmp/iosevka-term/ttf/*.ttf ${fonts}
 fi
 
 # Noto Sans
-if [ ! -f ~/.fonts/iosevka-term-regular.ttf ]; then
+if [ ! -f ${fonts}/NotoSans-Regular.ttf ]; then
   curl -fLo /tmp/noto-sans.zip https://noto-website-2.storage.googleapis.com/pkgs/NotoSans-hinted.zip
-	cd /tmp && unzip noto-sans.zip -d noto-sans
-  cp /tmp/noto-sans/*.ttf ~/.fonts
+  cd /tmp && unzip noto-sans.zip -d noto-sans
+  cp /tmp/noto-sans/*.ttf ${fonts}
 fi
 
-# Set up all of the configs:
-cd ${base}/stow
+# Refresh font cache
+fc-cache -v
 
-# This for loop iterates through all directories
-# contained in the stow directory. This makes
-# it easy to add configurations for new applications
-# without having to modify this script.
+# Symlink everything in stow directory to home directory
+cd ${base}/stow
 for app in */; do
 	stow -t ${HOME} $app
 done;
