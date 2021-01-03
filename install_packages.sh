5986d20404da61f8c327ea91e828a07c9396942f
Marc Gallant <marc.j.gallant@gmail.com>
2021-01-02 21:33:22 -0500

    Updated dconf settings.

--- install_packages.sh
+++ install_packages.sh
@@ -24,19 +24,3 @@ apt-get install -y \
   meld \
   python3-dev \
   papirus-icon-theme
-
-# Install jumpapp
-
-if [[ -a $(which jumpapp) ]]; then
-  echo "Jumpapp is already installed."
-  exit 0
-fi
-
-if [[ ! -a $(which curl) ]]; then
-  echo "Error: curl is not installed. Please install curl first."
-  exit 1
-fi
-
-curl -fLo /tmp/jumpapp.deb https://github.com/mkropat/jumpapp/releases/download/v1.1/jumpapp_1.1-1_all.deb
-cd /tmp
-apt install ./jumpapp.deb -y
