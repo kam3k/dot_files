3bb74f875f9c0cd62ccd55bbd44b3d93db92c0fa
Marc Gallant <marc.gallant@quanergy.com>
2026-06-19 14:05:23 -0400

    Added advanced media player gnome extension.

--- setup.sh
+++ setup.sh
@@ -165,6 +165,7 @@ my_extensions=(
   "live-lockscreen@nick-redwill"
   "SmartAutoMoveNG@lauinger-clan.de"
   "workspaces-by-open-apps@favo02.github.com"
+  "advanced-media-controller@sanjai.com"
 )
 
 # Install extensions using an isolated pipx environment runner
